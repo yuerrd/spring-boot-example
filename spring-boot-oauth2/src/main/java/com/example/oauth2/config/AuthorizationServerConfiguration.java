61dae084d1f225684a053e5f29e5335656599e77
yuerrd <yuerrd@gmail.com>
2019-12-18 21:05:26 +0800

    修改token

--- spring-boot-oauth2/src/main/java/com/example/oauth2/config/AuthorizationServerConfiguration.java
+++ spring-boot-oauth2/src/main/java/com/example/oauth2/config/AuthorizationServerConfiguration.java
@@ -1,28 +1,39 @@
 package com.example.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+/**
+ * @author yangyong
+ */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        // 配置客户端
-        clients
-                // 使用内存设置
-                .inMemory()
-                // client_id
-                .withClient("client")
-                // client_secret
-                .secret("secret")
-                // 授权类型
-                .authorizedGrantTypes("authorization_code")
-                // 授权范围
-                .scopes("app")
-                // 注册回调地址
-                .redirectUris("http://www.funtl.com");
+        clients.inMemory()
+                //配置client_id
+                .withClient( "client" )
+                //配置client_secret
+                .secret( passwordEncoder.encode( "secret" ) )
+                //配置访问token的有效期
+                .accessTokenValiditySeconds( 3600 )
+                //配置刷新token的有效期
+                .refreshTokenValiditySeconds( 864000 )
+                //配置redirect_uri，用于授权成功后跳转
+                .redirectUris( "http://www.baidu.com" )
+                //配置申请的权限范围
+                .scopes( "all" )
+                //配置grant_type，表示授权类型
+                .authorizedGrantTypes( "authorization_code" );
     }
 }
