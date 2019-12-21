d80b99a9f124a3f0864290dd38f5832209010a4d
yuerrd <yuerrd@gmail.com>
2019-12-21 17:30:56 +0800

    允许body认证

--- spring-boot-oauth2/src/main/java/com/example/oauth2/config/WebSecurityConfiguration.java
+++ spring-boot-oauth2/src/main/java/com/example/oauth2/config/WebSecurityConfiguration.java
@@ -1,12 +1,15 @@
 package com.example.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 /**
  * @author yangyong
@@ -15,6 +18,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    JsonToUrlEncodedAuthenticationFilter jsonFilter;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         // 设置默认的加密方式
@@ -33,7 +40,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser( "admin" )
                 .password( passwordEncoder().encode( "123456" ) )
-                .authorities("admin");
+                .authorities( "admin" );
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure( http );
+        http
+                .addFilterBefore( jsonFilter, ChannelProcessingFilter.class );
     }
 }
