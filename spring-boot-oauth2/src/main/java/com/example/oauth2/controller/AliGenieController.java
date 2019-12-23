e3978a260349938b8d62408372fe30b71e0959ff
yuerrd <yuerrd@gmail.com>
2019-12-24 01:12:31 +0800

    添加log

--- spring-boot-oauth2/src/main/java/com/example/oauth2/controller/AliGenieController.java
+++ spring-boot-oauth2/src/main/java/com/example/oauth2/controller/AliGenieController.java
@@ -1,5 +1,6 @@
 package com.example.oauth2.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.oauth2.common.HeaderNameEnum;
 import com.example.oauth2.common.NamespaceEnum;
 import com.example.oauth2.entity.ResponseDiscoveryEntity;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -28,7 +31,13 @@ public class AliGenieController {
     public ResponseDiscoveryEntity aligenie(@RequestParam Map<String, Object> params, HttpServletRequest request,
                                             HttpServletResponse response) {
         String namespace = request.getHeader("namespace");
+
         log.info("[ AliGenieController ] naesapce: {} ", namespace);
+
+        log.info("[ AliGenieController ] headers: {} ", JSON.toJSONString(getHeadersInfo(request)));
+        log.info("[ AliGenieController ] params: {}", JSON.toJSONString(params));
+
+
         if (StringUtils.equals(namespace, NamespaceEnum.DISCOVERY.toValue())) {
             response.setHeader("namespace", request.getHeader("namespace"));
             response.setHeader("name", HeaderNameEnum.DISCOVERY_DEVICES_RESPONSE.toValue());
@@ -37,5 +46,25 @@ public class AliGenieController {
             return deviceService.getDevices();
         }
         return null;
+    }
+
+
+    private Map<String, String> getHeadersInfo(HttpServletRequest request) {
+
+        Map<String, String> map = new HashMap<>(16);
+
+        Enumeration headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+
+            String key = (String) headerNames.nextElement();
+
+            String value = request.getHeader(key);
+
+            map.put(key, value);
+
+        }
+
+        return map;
     }
 }
