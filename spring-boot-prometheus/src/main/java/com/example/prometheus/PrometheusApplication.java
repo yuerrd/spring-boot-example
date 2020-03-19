522c89fe014f503c6f823438218f99bb09ffb2f1
yuerrd <yuerrd@gmail.com>
2020-03-19 15:19:56 +0800

    修改prom

--- spring-boot-prometheus/src/main/java/com/example/prometheus/PrometheusApplication.java
+++ spring-boot-prometheus/src/main/java/com/example/prometheus/PrometheusApplication.java
@@ -1,14 +1,23 @@
 package com.example.prometheus;
 
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrometheusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run( PrometheusApplication.class, args );
+    }
+
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "spring-boot-prometheus");
     }
 
 }
